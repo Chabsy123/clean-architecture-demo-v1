263a1f1525ff8f4eadfea18ad4518a8a0b1a688a
Chukwu Sydney <chukwusydney061@gmail.com>
2026-03-11 01:51:48 +0100

    Setup 4-Layer Clean Architecture with Modular DI.

---
+++ clean-architecture-demo-v1-api/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using clean_architecture_demo_v1.App;
+using clean_architecture_demo_v1.Framing;
+
+namespace clean_architecture_demo_v1_api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAppDI(this IServiceCollection services)
+        {
+            services.AddApplicationDI()
+                .AddFramingDI();
+            return services;
+        }
+    }
+}
---
+++ clean-architecture-demo-v1.Framing/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clean_architecture_demo_v1.Framing
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddFramingDI(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
